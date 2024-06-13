d6fa93fc99f7b81e7a588ffc03a8de1be5f0306b
Deepaksaini73 <Deepakrajaji73@gmail.com>
2024-06-13 07:25:09 +0530

    first commit

---
+++ js projects/1st web project/Number Game css.js
@@ -0,0 +1,23 @@
+let a=Math.floor((Math.random())*100);  
+let b=document.getElementById("number");
+function clik(){
+    let d=b.value;
+  console.log(d);
+}
+
+
+// var element = document.querySelector(".toptext"); 
+// element.innerText = "Try Again ! ";
+
+
+// do{
+//   n=Number.parseInt(n);
+//   if(a==n){
+//     break;
+//   }
+//   console.log("given number is "+ (n>a? "greater than random number":"less than random number"));
+//   count++;
+// }
+//   while(a!=n);
+
+// console.log("your score is "+ (100-count));
